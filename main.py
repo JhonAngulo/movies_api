9a602e1ab629f4c1f85a5cec03e96d9d6a802ede
JhonAngulo <jhonmanuelm89@gmail.com>
2024-06-11 21:24:43 -0500

    validate endpoints

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
-from fastapi import FastAPI, Body
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Body, Path, Query
+from fastapi.responses import HTMLResponse, JSONResponse
+from pydantic import BaseModel, Field
+from typing import Optional, List
 
 
 app = FastAPI()
 app.title = "Mi primera aplicación con FastAPI"
 app.version = "1.0.0"
+
+
+class Movie(BaseModel):
+    id: Optional[int] = None
+    title: str = Field(min_length=5, max_length=30)
+    overview: str = Field(min_length=15, max_length=100)
+    year: int = Field(le=2022)
+    rating: float = Field(ge=1, le=10)
+    category: str  = Field(min_length=5, max_length=15)
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "id": 3,
+                    "title": "Título de la película",
+                    "overview": "Descripción de la película...",
+                    "year": "2022",
+                    "rating": 9.8,
+                    "category": "Humor"
+                }
+            ]
+        }
+    }
 
 movies = [
     {
@@ -29,29 +55,45 @@ movies = [
 def message():
     return HTMLResponse('<h1>¡Bienvenido a mi primera aplicación con FastAPI!</h1>')
     
-@app.get("/movies", tags=["Movies"])
-def get_movies():
-    return movies
+@app.get("/movies", tags=["Movies"], response_model=List[Movie], status_code=200)
+def get_movies() -> List[Movie]:
+    return JSONResponse(content=movies, status_code=200)
 
-@app.get("/movies/{id}", tags=["Movies"])
-def get_movie(id: int):
+@app.get("/movies/{id}", tags=["Movies"], response_model=Movie, status_code=200)
+def get_movie(id: int = Path(ge=1, le=2000)) -> Movie:
     for movie in movies:
         if movie["id"] == id:
-            return movie
-    return []
+            return JSONResponse(content=movie, status_code=200)
+    return JSONResponse(content=[], status_code=404)
 
-@app.get("/movies/", tags=["Movies"])
-def get_movies_by_category(category: str, year: int):
-    return [movie for movie in movies if movie["category"] == category ]
+@app.get("/movies/", tags=["Movies"], response_model=List[Movie])
+def get_movies_by_category(category: str = Query(min_length=5, max_length=15)) -> List[Movie]:
+    data = [movie for movie in movies if movie["category"] == category]
+    return JSONResponse(content=data)
 
-@app.post("/movies", tags=["Movies"])
-def create_movie(id: int = Body(), title: str = Body(), overview: str = Body(), year: int = Body(), rating: float = Body(), category: str = Body()):
-    movies.append({
-        "id": id,
-        "title": title,
-        "overview": overview,
-        "year": year,
-        "rating": rating,
-        "category": category
-    })
-    return movies
+@app.post("/movies", tags=["Movies"], response_model=dict, status_code=201)
+def create_movie(movie: Movie) -> dict:
+    movies.append(movie)
+    return JSONResponse(content={"message": "Movie created successfully"}, status_code=201)
+
+
+@app.put("/movies/{id}", tags=["Movies"], response_model=dict, status_code=200)
+def update_movie(id: int, movie: Movie) -> dict:
+    for item in movies:
+        if item["id"] == id:
+            item["title"] = movie.title
+            item["overview"] = movie.overview
+            item["year"] = movie.year
+            item["rating"] = movie.rating
+            item["category"] = movie.category
+            return JSONResponse(content={"message": "Movie updated successfully"}, status_code=200)
+    return JSONResponse(content={"message": "Movie not found"}, status_code=404)
+
+
+@app.delete("/movies/{id}", tags=["Movies"], response_model=dict, status_code=200)
+def delete_movie(id: int) -> dict:
+    for movie in movies:
+        if movie["id"] == id:
+            movies.remove(movie)
+            return JSONResponse(content={"message": "Movie deleted successfully"}, status_code=200)
+        return JSONResponse(content={"message": "Movie not found"}, status_code=404)
